966d90ac9a22587823a591c4ea297ea6e561fec7
usama munir <70121208@student.uol.edu.pk>
2025-09-11 22:53:18 +0500

    signup page added

--- ecommerce/authApp/views.py
+++ ecommerce/authApp/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, HttpResponse
 
 # Create your views here.
 
 def signup(request):
-	return render(request, 'authentication/signUp.html')
+	
+	if request.method == 'POST':
+		email = request.POST['email']
+		password = request.POST['pass1']
+		confirm_password = request.POST['pass2']
+
+		if password != confirm_password:
+			return HttpResponse('password incorrect')
+
+		try:
+			if User.objects.get(username = email):
+				return HttpResponse('email already exist')
+		except Exception as e:
+			pass
+
+		user = User.objects.create_user(email,email,password)
+		user.save()
+		return HttpResponse('user created', email)
+
+	return render(request, 'signup.html')
+		
+
 
 def handlelogin(request):
 	return render(request, 'authentication/handlelogin.html')
